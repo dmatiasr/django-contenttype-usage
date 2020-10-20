e20321fb0d6a5eca18d102850714c3c9f747156d
rondeaumatias <mrondeau@ignetworks.com>
2020-10-20 17:25:57 -0300

    Content type serialization GET

--- generic_relation_api/api/serializers.py
+++ generic_relation_api/api/serializers.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from rest_framework import serializers, validators
 
 from module1.models import (
@@ -19,23 +21,77 @@ class TaggedObjectRelatedField(serializers.RelatedField):
 
         return serializer.data
 
-class BookmarkSerializer(serializers.ModelSerializer):
-    #tags = TaggedObjectRelatedField(many=True, queryset=TaggedItem.objects.all())
+class TaggedObjectTypeRelatedField(serializers.RelatedField):
+    
+    def to_representation(self, value):
 
+        if isinstance(value, Bookmark):
+            return 'bookmark'
+        elif isinstance(value, Note):
+            return 'note'
+        raise Exception('Unexpected type of tagged object')
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Bookmark 
-        fields = '__all__'
+        fields = ('slug','url')
+    
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = [field for field in self.fields.values() if not field.write_only]
 
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is None:
+                # We skip `to_representation` for `None` values so that
+                # fields do not have to explicitly deal with that case.
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+        ret['content_name'] = instance.__class__.__name__.lower()
+
+        return ret
+ 
 class NoteSerializer(serializers.ModelSerializer):
-    #tags = TaggedObjectRelatedField(many=True, queryset=TaggedItem.objects.all())
     
     class Meta:
         model = Note
-        fields = '__all__'
+        fields = ('slug', 'text')
+
+    def to_representation(self, instance):
+        """
+        Object instance -> Dict of primitive datatypes.
+        """
+        ret = OrderedDict()
+        fields = [field for field in self.fields.values() if not field.write_only]
+
+        for field in fields:
+            try:
+                attribute = field.get_attribute(instance)
+            except SkipField:
+                continue
+
+            if attribute is None:
+                # We skip `to_representation` for `None` values so that
+                # fields do not have to explicitly deal with that case.
+                ret[field.field_name] = None
+            else:
+                ret[field.field_name] = field.to_representation(attribute)
+        ret['content_name'] = instance.__class__.__name__.lower()
+
+        return ret
 
 
 class TaggedItemSerializer(serializers.ModelSerializer):
     tagged_object = TaggedObjectRelatedField(queryset=TaggedItem.objects.all())
     class Meta:
         model = TaggedItem
-        fields = ('id', 'slug', 'tagged_object')
+        fields = ('slug', 'tagged_object')
